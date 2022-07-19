6ec889b769987708936631f9450f97d445c7cc01
phikhanh1107 <106798845+khanhhpse150409@users.noreply.github.com>
2022-07-19 15:17:39 +0700

    React Animation Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -7,6 +7,8 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
+
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -31,7 +33,8 @@ class CommentForm extends Component {
    
    handleSubmit(values) {
       this.toggleModal();
-      this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+      // this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+      this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
       // console.log("Current State is: " + JSON.stringify(values));
       // alert("Current State is: " + JSON.stringify(values));
    }
@@ -113,36 +116,49 @@ class CommentForm extends Component {
 function RenderDish({dish}) {
    return (
       <div className="col-12 col-md-5 m-1">
-         <Card>
-            {/* <CardImg top src={dish.image} alt={dish.name} /> */}
-            <CardImg top src={baseUrl + dish.image} alt={dish.name} />
-            <CardBody>
-               <CardTitle><h5 class="mb-2 text-info">{dish.name}</h5></CardTitle>
-               <CardText>{dish.description}</CardText>
-            </CardBody>
-         </Card>
+         <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+            <Card>
+                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+                <CardBody>
+                     <CardTitle><h5 class="mb-2 text-info">{dish.name}</h5></CardTitle>
+                     <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+         </FadeTransform>
       </div>
    );
 }
    
 // function RenderComments({comments}) {
-function RenderComments({comments, addComment, dishId}) {   
+// function RenderComments({comments, addComment, dishId}) {   
+function RenderComments({comments, postComment, dishId}) {
    if (comments != null) {
       return (
          <div className="col-12 col-md-5 m-1">
             <h4 class="mb-2 text-info">Comments</h4>
+
             <ul className="list-unstyled">
-            {comments.map((comment) => {
-               return (
-                  <li key={comment.id}>
-                    <p>{comment.comment}</p>
-                    <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>                                     
-                  </li>
-               );
-            })}
+
+               <Stagger in>
+                  {comments.map((comment) => {
+                     return (
+                        <Fade in>
+                           <li key={comment.id}>
+                              <p>{comment.comment}</p>
+                              <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                           </li>
+                        </Fade>         
+                     );
+                  })}
+               </Stagger>
             </ul>
             {/* <CommentForm /> */}
-            <CommentForm dishId={dishId} addComment={addComment} />
+            {/* <CommentForm dishId={dishId} addComment={addComment} /> */}
+            <CommentForm dishId={dishId} postComment={postComment} />
          </div>
       );
    }
@@ -152,32 +168,6 @@ function RenderComments({comments, addComment, dishId}) {
       );
    }
 }
-   
-// function RenderComments({comments}) {
- // if (comments != null) {
-	// return (
-	   // <div className="col-12 col-md-5 m-1">
-		  // <h4>Comments</h4>
-		  // <ul className="list-unstyled">
-		  // {comments.map((comment) => {
-			 // return (
-				// <li key={comment.id}>
-				  // <p>{comment.comment}</p>
-				  // <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-				// </li>
-			 // );
-		   // })}
-		   // </ul>
-	   // </div>
-	// );
- // }
- // else {
-	// return (
-	   // <div></div>
-	// );
- // }
- 
-// }
 
 const DishDetail = (props) => {
    if (props.isLoading) {
@@ -217,7 +207,8 @@ const DishDetail = (props) => {
                <RenderDish dish={props.dish}/>
                {/* <RenderComments comments={props.comments}/> */}
                <RenderComments comments={props.comments}
-                  addComment={props.addComment}
+                  // addComment={props.addComment}
+                  postComment={props.postComment}
                   dishId={props.dish.id}
                />
             </div>
